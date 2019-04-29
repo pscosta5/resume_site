e93e80f5d43a03cc95bd3579efd254a975b5a150
Paulo S. Costa <pauloscosta5@gmail.com>
2019-04-28 20:21:16 -0700

    Added homepage

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, url_for, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@app.route("/communication")
+def communication():
+    return render_template(("communication.html"))
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(debug=True)
