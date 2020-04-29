9b159d6bc39017d075b24b8948dc93f7fab738cf
Paulo Costa <pcosta@firstrepublic.com>
2020-04-29 13:49:41 -0700

    Lint

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask, request, url_for, render_template, redirect
+"""Web app."""
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template("layout.html")
+def index() -> str:
+    """Base page."""
+    return flask.render_template("layout.html")
 
 
 @app.route("/about")
-def about():
-    return render_template("about.html")
+def about() -> str:
+    """Route to about page."""
+    return flask.render_template("about.html")
 
 
 @app.route("/projects")
-def projects():
-    return render_template("projects.html")
+def projects() -> str:
+    """Route to projects page."""
+    return flask.render_template("projects.html")
 
 
 @app.route("/communication")
-def communication():
-    return render_template(("communication.html"))
+def communication() -> str:
+    """Route to communication page."""
+    return flask.render_template(("communication.html"))
 
 
 if __name__ == "__main__":
